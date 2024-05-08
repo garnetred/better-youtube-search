09f13268ababcb750579d8c4cec0ab90f8b96b83
December G <dgarnetsmith@gmail.com>
2024-05-07 23:52:36 -0500

    Add tabs permission and add try catch block to insert browser scripting

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,21 @@
-console.log('in background.js')
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(tabId, changeInfo, tab)
+console.log("in background.js");
+browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  console.log(tabId, changeInfo, tab);
   const tabUrl = tab.url ?? tab.pendingUrl;
-  console.log('tabURL', tabUrl)
+  console.log("tabURL", tabUrl);
   if (
-    changeInfo.status === 'complete' &&
+    changeInfo.status === "complete" &&
     tabUrl &&
-    (tabUrl.includes('youtube.com/results'))
+    tabUrl.includes("youtube.com/results")
   ) {
-    console.log('in if statement')
-    browser.scripting.insertCSS({
-      target: { tabId: tabId },
-      files: ['css/global.css'],
-    });
+    console.log("in if statement");
+    try {
+      await browser.scripting.insertCSS({
+        target: { tabId: tabId },
+        files: ["css/global.css"],
+      });
+    } catch (err) {
+      console.error(`Could not insert css file due to ${err}`);
+    }
   }
 });
