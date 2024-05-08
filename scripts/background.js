1b056e671dabcb80f3ea5e810c4ba45bbf3fe2c0
December G <dgarnetsmith@gmail.com>
2024-05-08 00:11:36 -0500

    Remove console logs

--- scripts/background.js
+++ scripts/background.js
@@ -1,14 +1,10 @@
-console.log("in background.js");
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  console.log(tabId, changeInfo, tab);
   const tabUrl = tab.url ?? tab.pendingUrl;
-  console.log("tabURL", tabUrl);
   if (
     changeInfo.status === "complete" &&
     tabUrl &&
     tabUrl.includes("youtube.com/results")
   ) {
-    console.log("in if statement");
     try {
       await chrome.scripting.insertCSS({
         target: { tabId: tabId },
