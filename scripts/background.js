beb0e3ccdbd2681d044052f8349bc07c22ffd925
December G <dgarnetsmith@gmail.com>
2024-05-07 23:41:05 -0500

    Request tab permissions, update chrome extension id, and add background scripts property

--- scripts/background.js
+++ scripts/background.js
@@ -1,11 +1,15 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+console.log('in background.js')
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log(tabId, changeInfo, tab)
   const tabUrl = tab.url ?? tab.pendingUrl;
+  console.log('tabURL', tabUrl)
   if (
     changeInfo.status === 'complete' &&
     tabUrl &&
     (tabUrl.includes('youtube.com/results'))
   ) {
-    chrome.scripting.insertCSS({
+    console.log('in if statement')
+    browser.scripting.insertCSS({
       target: { tabId: tabId },
       files: ['css/global.css'],
     });
